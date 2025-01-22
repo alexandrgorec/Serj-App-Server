348b1db89df442af91fcccdcb08e8e494e1f9cca
Alexandr Goryainov <2400sa@gmail.com>
2025-01-22 22:23:44 +0300

    add build

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser= require("body-parser")
+const bodyParser = require("body-parser")
+const path = require('path');
 const PORT = 3001;
-
+const STATIC_PATH = path.join(__dirname, "./build");
+app.use(express.static(STATIC_PATH));
 app.use(cors());
 
 
-const orders =  [];
+
+
+const orders = [];
 
 app.post("/neworder", bodyParser.json(), (req, res) => {
     orders.push(req.body);
@@ -20,6 +24,11 @@ app.post("/getallorders", bodyParser.json(), (req, res) => {
     console.log("getAllOrders:");
     console.log(orders);
     res.send(orders);
+})
+
+app.get("/", (req, res) => {
+    console.log("connected");
+    res.sendFile(path.join(STATIC_PATH, 'index.html'));
 })
 
 
