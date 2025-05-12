69432a65f87d5297c101611bf907894d0ada7c5f
Alexandr_Goryaynov <2400sa@gmail.com>
2025-05-12 21:59:44 +0300

    add delete User

--- index.js
+++ index.js
@@ -42,27 +42,27 @@ const checkAuth = (req, res, next) => {
 
 app.get("/test", (req, res) => {
 
-    pool.query("select orderjson from orders where id = $1", [1], (err, result) => {
+    pool.query("select * from users ", (err, result) => {
         if (err) {
             console.error('Error connecting to the database', err.stack);
             res.send('ошибка доступа к базе данных');
         } else {
-            const result1 = result.rows[0];
-            res.send(result1.orderjson);
+            const result1 = result.rows;
+            res.send(result1);
         }
     });
 
 
-    //  pool.query("UPDATE users SET userinfo = $1 where id = $2", [{name:"Менеджер Валера"}, 1], (err, result) => {
-    //                 if (err) {
-    //                     console.error('Error connecting to the database', err.stack);
-    //                     res.send('ошибка доступа к базе данных');
-    //                 } else {
-    //                     console.log('EDIT userSelectListsData');
+    // pool.query("UPDATE users SET userinfo = $1 where id = $2", [{ name: "Менеджер Валера" }, 1], (err, result) => {
+    //     if (err) {
+    //         console.error('Error connecting to the database', err.stack);
+    //         res.send('ошибка доступа к базе данных');
+    //     } else {
+    //         console.log('EDIT userSelectListsData');
 
-    //                     res.sendStatus(202);
-    //                 }
-    //             });
+    //         res.sendStatus(202);
+    //     }
+    // });
 
 
 })
@@ -70,74 +70,106 @@ app.get("/test", (req, res) => {
 
 app.post('/getAccessToken', (req, res) => {
     const selectListsData = {
-        SUPPLIERS: ['РНК', "Барс", "Грасс", "Юма"],
-        BUYERS: ['Техресурс', "Слава", "Мостпроект", "тройка-тт", "Тайга", "Сеч", "Горбунов"],
-        DRIVERS: ['Василий Иванович', "Григорий", "Валера", "Анатолий Степанович"],
-        TYPE_OF_PRODUCT: ['ДТ-Е-К5', "Дизель", "АИ-95", "GT-POWER"],
-        MANAGERS: ['Антон', "Сержан", "Семён", "Иванов"],
+        SUPPLIERS: [],
+        BUYERS: [],
+        DRIVERS: [],
+        TYPE_OF_PRODUCT: [],
+        MANAGERS: [],
     }
 
     const sql = 'select * from users where login = $1';
-    pool.query(sql, [req.body.u], (err, result) => {
-        if (err) {
-            res.send("ошибка доступа к базе данных")
+
+    if (req.body.u === 'root') {
+        if (req.body.p === process.env.SECRET_KEY) {
+            res.status(202);
+            const payload = {
+                user: {
+                    name: 'root',
+                    rights: {
+                        finBlockAccess: true,
+                        adminAccess: true,
+                    },
+                    selectListsData: selectListsData,
+                    userId: 'root',
+                },
+            }
+            const token = jwt.sign(payload, process.env.SECRET_KEY, {
+                expiresIn: 60 * 60 * 24 * 7,
+            });
+            res.send(token);
         }
         else {
-            if (result.rowCount !== 0) {
-                const user = result.rows[0];
-                if (req.body.p === user.password) {
-                    res.status(202);
-                    const payload = {
-                        user: {
-                            name: user?.userinfo?.name || '',
-                            rights: user.rights,
-                            selectListsData: user?.userinfo?.selectListsData || selectListsData,
-                            userId: user.id,
-                        },
-                    }
-                    const token = jwt.sign(payload, process.env.SECRET_KEY, {
-                        expiresIn: 60 * 60 * 24 * 7,
-                    });
-                    res.send(token);
-                }
-                else {
-                    res.send('Ошибка авторизации'); // неверный пароль
-                }
+            res.send('Ошибка авторизации'); // неверный пароль
+        }
+
+    }
+    else {
+        pool.query(sql, [req.body.u], (err, result) => {
+            if (err) {
+                res.send("ошибка доступа к базе данных")
             }
             else {
-                res.send("Ошибка авторизации") // такого опльзователя не существует
+                if (result.rowCount !== 0) {
+                    const user = result.rows[0];
+                    if (req.body.p === user.password) {
+                        res.status(202);
+                        const payload = {
+                            user: {
+                                name: user?.userinfo?.name || '',
+                                rights: user.rights,
+                                selectListsData: user?.userinfo?.selectListsData || selectListsData,
+                                userId: user.id,
+                            },
+                        }
+                        const token = jwt.sign(payload, process.env.SECRET_KEY, {
+                            expiresIn: 60 * 60 * 24 * 7,
+                        });
+                        res.send(token);
+                    }
+                    else {
+                        res.send('Ошибка авторизации'); // неверный пароль
+                    }
+                }
+                else {
+                    res.send("Ошибка авторизации") // такого пользователя не существует
+                }
             }
-        }
-    })
+        })
+    }
 });
 
 
 app.use(express.static(STATIC_PATH));
 
 
-// app.get("/createuser", (req, res) => {
-//     const rights = {};
-//     const codeUserExist = 23505;
-//     rights.finBlockAccess = true;
-//     const login = '123';
-//     const password = '123';
-//     const userinfo = {
-//         name: "Бухгалтер Галина",
-//     }
-//     const insertText = 'INSERT INTO users(login, password, rights, userinfo) VALUES ($1, $2, $3, $4)';
-//     pool.query(insertText, [login, password, rights, userinfo], (err, result) => {
-//         if (err)
-//             if (err.code = codeUserExist)
-//                 res.send(`Пользователь ${login} уже существует`);
-//             else
-//                 res.send('Ошибка')
-//         if (result)
-//             res.send('пользователь создан');
-//     });
-
-// })
-
 app.use(checkAuth);
+
+app.post("/adduser", (req, res) => {
+    const rights = req.body.newUser.rights;
+    const login = req.body.newUser.login;
+    const password = req.body.newUser.password;
+    const userInfo = req.body.newUser.userInfo;
+    const codeUserExist = 23505;
+    const insertText = 'INSERT INTO users (login, password, rights, userinfo) VALUES ($1, $2, $3, $4)';
+    pool.query(insertText, [login, password, rights, userInfo], (err, result) => {
+        if (err) {
+            if (err.code = codeUserExist) {
+                res.status(203);
+                res.send(`Пользователь ${login} уже существует`);
+            }
+            else {
+                res.status(203);
+                res.send(`Неизвестная ошибка`);
+            }
+        }
+        if (result) {
+            res.status(202);
+            res.send(`Пользователь ${login} создан`);
+        }
+
+    });
+
+})
 
 app.post("/getData", (req, res) => {
     res.status(202);
@@ -154,8 +186,8 @@ app.post("/getData", (req, res) => {
 
 app.post("/neworder", (req, res) => {
     console.log("connected")
-    const order = req.body.order; 
-    
+    const order = req.body.order;
+
     const date = new Date();
     let dd = date.getDate();
     if (dd < 10) dd = '0' + dd;
@@ -195,9 +227,37 @@ app.post("/getallorders", (req, res) => {
     });
 })
 
+app.post("/getListUsers", (req, res) => {
+    pool.query("select id, rights,  login, userinfo from users",  (err, result) => {
+        if (err) {
+            console.error('Error connecting to the database', err.stack);
+            res.send('ошибка доступа к базе данных');
+        } else {
+            res.status(202);
+            const answer = {}
+            answer.users = result.rows.filter( user => user.id !== req.body.userId);
+            res.json(answer);
+        }
+    });
+})
+
 app.post("/deleteorder", (req, res) => {
     const id = req.body.id;
     pool.query("delete from orders where id = $1", [id], (err, result) => {
+        if (err) {
+            console.error('Error delete order', err.stack);
+            res.send('ошибка доступа к базе данных');
+        } else {
+            res.sendStatus(202);
+        }
+    });
+})
+
+app.post("/deleteuser", (req, res) => {
+    // console.log(req.body);
+    const id = +req.body.deleteUser.id;
+    console.log("userIdDelete:", id);
+    pool.query("delete from users where id = $1", [id], (err, result) => {
         if (err) {
             console.error('Error delete order', err.stack);
             res.send('ошибка доступа к базе данных');
